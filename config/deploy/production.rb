cd27832931ab588649dcb2e6f8966625ad237e69
vagrant <vagrant@vagrant-centos65.vagrantup.com>
2016-08-24 23:22:07 +0000

    Modify

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,6 +6,6 @@ role :web, %w{vagrant@192.168.33.21}
 role :db, %w{vagrant@192.168.33.21}
 
 set :ssh_options, {
-  keys: [File.expand_path('~/.ssh/insecure_private_key')],
+  keys: [File.expand_path('~/.ssh/pro_private_key')],
   forward_agent: true
 }
